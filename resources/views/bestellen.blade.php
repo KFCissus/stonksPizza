5d5057a82ded6ac18f59567f2284f271467ea9d7
Aiden <103739543+AidenistooOP@users.noreply.github.com>
2024-01-14 22:16:25 +0100

    This is the new version of the order page with some basic style scheme.

--- resources/views/bestellen.blade.php
+++ resources/views/bestellen.blade.php
@@ -5,10 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bestellen - Pizza</title>
-    @vite('resources/css/app.css')
+    @vite('resources/css/bestellen.css')
     <script src="https://kit.fontawesome.com/80bfb3b6e2.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+ <div id="productContainer">
+    <div id="pepperoniPizza">
+        <img src="https://static.vecteezy.com/system/resources/previews/010/793/986/original/pepperoni-pizza-isolated-with-clipping-path-png.png" rel="pepperoni">
+        <p>Deze pepperoni pizza is super duper duper lekker, dus koop deze nu en het wordt snel bezorgd.</p>
+        <button><i class="fa-solid fa-cart-shopping"></i> Toevoegen</button>
+    </div>
+    <div id="hawaiPizza">
+        <img src="https://images.newyorkpizza.nl/Products/Original/Hawaii-8059.png" rel="pepperoni">
+        <p>Deze hawai pizza is super duper duper lekker, dus koop deze nu en het wordt snel bezorgd.</p>
+        <button><i class="fa-solid fa-cart-shopping"></i> Toevoegen</button>
+    </div>
+    <div id="olijfPizza">
+        <img src="https://www.freeiconspng.com/thumbs/pizza-png/pizza-png-15.png" rel="pepperoni">
+        <p>Deze pepperoni pizza is super duper duper lekker, dus koop deze nu en het wordt snel bezorgd.</p>
+        <button><i class="fa-solid fa-cart-shopping"></i> Toevoegen</button>
+    </div>
+    <div id="pepperoniPizza">
+        <img src="https://static.vecteezy.com/system/resources/previews/010/793/986/original/pepperoni-pizza-isolated-with-clipping-path-png.png" rel="pepperoni">
+        <p>Deze pepperoni pizza is super duper duper lekker, dus koop deze nu en het wordt snel bezorgd.</p>
+        <button><i class="fa-solid fa-cart-shopping"></i> Toevoegen</button>
+    </div>
+ </div>
 </body>
 </html>
