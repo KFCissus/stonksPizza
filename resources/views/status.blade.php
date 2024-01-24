68e9a086ec72d0e36d20e257055cba5e569f88fd
Aiden <103739543+AidenistooOP@users.noreply.github.com>
2024-01-24 08:46:46 +0100

    Added the status page with functionality

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -5,8 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Status bestelling</title>
+    @vite('resources/css/status.css')
+    <script src="https://kit.fontawesome.com/80bfb3b6e2.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <h1>Pizza Tracking</h1>
+    <div id="statusGrid">
+        <div id="columnBetaald">
+            <h1>Betaald</h1>
+            <i class="fa-solid fa-money-check-dollar"></i>
+        </div>
+        <div id="columnBereiden">
+            <h1>Bereiden</h1>
+            <i class="fa-solid fa-pizza-slice"></i>
+        </div>
+        <div id="columnBesteld">
+            <h1>In de oven</h1>
+            <i class="fa-solid fa-fire"></i>
+        </div>
+        <div id="columnBesteld">
+            <h1>Onderweg</h1>
+            <i class="fa-solid fa-truck-front"></i>
+        </div>
+    </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            setTimeout(function () {
+                document.getElementById("columnBetaald").classList.add("green-border");
+            }, 5000);
+
+            setTimeout(function () {
+                document.getElementById("columnBereiden").classList.add("green-border");
+            }, 25000);
+        });
+    </script>
 </body>
 </html>
